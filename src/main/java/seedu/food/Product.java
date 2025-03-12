3ad47ddbc3bee3e5c95ae6164a710c8ef22367ce
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-12 09:43:52 +0800

    Create an abstract class called 'Product'
    This will be the parent class for 'Ingredient' and 'Meal' classes.

---
+++ src/main/java/seedu/food/Product.java
@@ -0,0 +1,30 @@
+package seedu.food;
+
+import seedu.exceptions.InvalidIngredientPriceException;
+import seedu.exceptions.InvalidPriceException;
+
+public abstract class Product {
+    protected String name;
+    protected double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) throws InvalidPriceException {
+        int zeroDollars = 0;
+        String name = getName();
+        if (price <= zeroDollars) {
+            throw new InvalidPriceException(name);
+        }
+        this.price = price;
+    }
+}
