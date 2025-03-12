293b327ae2a5d8d2d6e93e74783f1bb9033f0718
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-12 09:46:19 +0800

    Add an exception for checking duplicate ingredients in a meal

---
+++ src/main/java/seedu/exceptions/DuplicateIngredientException.java
@@ -0,0 +1,16 @@
+package seedu.exceptions;
+
+public class DuplicateIngredientException extends EZMealPlanException {
+    String ingredientName;
+    String mealName;
+
+    public DuplicateIngredientException(String ingredientName, String mealName) {
+        this.ingredientName = ingredientName;
+        this.mealName = mealName;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Ingredient " + this.ingredientName + " already exists in the meal " + this.mealName + ".";
+    }
+}
