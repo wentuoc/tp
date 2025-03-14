ea9ebb5daf11d53d1f7c354fbed24bbc416ca622
Cheng-Zhiyuan <e1138578@u.nus.edu>
2025-03-14 15:35:14 +0800

    Fix bug of appending new line

--- src/main/java/seedu/ui/UserInterface.java
+++ src/main/java/seedu/ui/UserInterface.java
@@ -19,7 +19,7 @@ public class UserInterface {
     }
 
     public void printGoodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("Bye. Hope to see you again soon!");
     }
 
     public void printUnknowCommand() {
