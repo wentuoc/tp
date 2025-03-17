9a6a73d7be53c4242f48081afaed1fb6195ee132
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-17 14:13:37 +0800

    Update Command class with setter and getter for validUserInput

--- src/main/java/seedu/command/Command.java
+++ src/main/java/seedu/command/Command.java
@@ -1,13 +1,23 @@
 package seedu.command;
 
+import seedu.exceptions.EZMealPlanException;
 import seedu.logic.MealManager;
 import seedu.ui.UserInterface;
 
 public abstract class Command {
+    protected String validUserInput;
 
     public boolean isExit() {
         return false;
     }
 
-    public abstract void execute(MealManager mealManager, UserInterface ui);
+    public void setValidUserInput(String validUserInput) {
+        this.validUserInput = validUserInput;
+    }
+
+    public String getValidUserInput() {
+        return validUserInput;
+    }
+
+    public abstract void execute(MealManager mealManager, UserInterface ui) throws EZMealPlanException;
 }
