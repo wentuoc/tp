0feed3aafc6c52a3448a0f062ee0b8d6a8f3f7ff
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-17 15:27:51 +0800

    Remove unnecessary lines

--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -3,21 +3,17 @@ package seedu.parser;
 import seedu.command.ByeCommand;
 import seedu.command.Command;
 import seedu.command.CreateCommand;
-import seedu.command.SelectCommand;
 import seedu.command.UnknownCommand;
 
 public class Parser {
     public static Command parse(String userInput) {
         String bye = "bye";
         String create = "create";
-        String select = "select";
         userInput = userInput.toLowerCase().trim();
         if (userInput.contains(bye)) {
             return new ByeCommand();
         } else if (userInput.contains(create)) {
             return new CreateCommand(userInput);
-        } else if (userInput.contains(select)) {
-            return new SelectCommand(userInput);
         }
         return new UnknownCommand(userInput);
     }
