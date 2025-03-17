ae822ee711835431c4f7a30c4c8d8f81e9647209
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-17 14:10:21 +0800

    Rename from Duke to EZMealPlan

---
+++ src/test/java/seedu/ezmealplan/EZMealPlanTest.java
@@ -0,0 +1,12 @@
+package seedu.ezmealplan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EZMealPlanTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
