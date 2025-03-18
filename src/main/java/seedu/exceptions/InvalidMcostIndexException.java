67189ff547d5a44c9721079a005b71003c1f04fa
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-19 00:37:51 +0800

    Include exceptions relevant to SelectCommand, FilterCommand and others

---
+++ src/main/java/seedu/exceptions/InvalidMcostIndexException.java
@@ -0,0 +1,14 @@
+package seedu.exceptions;
+
+public class InvalidMcostIndexException extends EZMealPlanException {
+    String filterOrSelect;
+
+    public InvalidMcostIndexException(String filterOrSelect) {
+        this.filterOrSelect = filterOrSelect;
+    }
+
+    @Override
+    public String getMessage() {
+        return "The /mcost keyword must appear after the '" + filterOrSelect + "' keyword command.";
+    }
+}
---
+++ src/main/java/seedu/exceptions/InvalidSelectIndexException.java
@@ -0,0 +1,9 @@
+package seedu.exceptions;
+
+public class InvalidSelectIndexException extends EZMealPlanException {
+    @Override
+    public String getMessage() {
+        return "The 'select' index input must be parsable into an integer and the resulting integer value\n" +
+                "must not be out of bounds.";
+    }
+}
