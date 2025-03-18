7175950a2b1d062dd778a8299c27c17423472ae4
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-19 00:50:20 +0800

    Account for the filter and select command by adding relevant methods

--- src/main/java/seedu/logic/MealManager.java
+++ src/main/java/seedu/logic/MealManager.java
@@ -2,9 +2,12 @@ package seedu.logic;
 
 import seedu.exceptions.DuplicateMealException;
 import seedu.exceptions.EZMealPlanException;
+import seedu.food.Ingredient;
 import seedu.food.Meal;
+import seedu.food.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MealManager {
@@ -22,6 +25,7 @@ public class MealManager {
     public void add(Meal newMeal, List<Meal> mealList) throws EZMealPlanException {
         checkDuplicateMeal(newMeal, mealList);
         mealList.add(newMeal);
+        mealList.sort(Comparator.comparing(Product::getName));
     }
 
     public void checkDuplicateMeal(Meal newMeal, List<Meal> mealList) throws EZMealPlanException {
@@ -37,5 +41,86 @@ public class MealManager {
                 throw new DuplicateMealException(mealName, listName);
             }
         }
+    }
+
+    public List<Meal> filteringByMcost(double mcostDouble) {
+        List<Meal> filteredMealList = new ArrayList<>();
+        List<Meal> mainMealList = getMainMealList();
+        for (Meal meal : mainMealList) {
+            if (meal.getPrice() == mcostDouble) {
+                filteredMealList.add(meal);
+            }
+        }
+        return filteredMealList;
+    }
+
+    public List<Meal> filteringByMname(String[] mealNameArray) {
+        List<Meal> filteredMealList = new ArrayList<>();
+        List<Meal> mainMealList = getMainMealList();
+        for (Meal meal : mainMealList) {
+            boolean isMatchingMname = checkMname(mealNameArray, meal);
+            if (isMatchingMname) {
+                filteredMealList.add(meal);
+            }
+        }
+        return filteredMealList;
+    }
+
+    private static boolean checkMname(String[] mealNameArray, Meal meal) {
+        boolean isMealNameContains = true;
+        String getMealName = meal.getName();
+        for (String mealName : mealNameArray) {
+            if (!getMealName.contains(mealName)) {
+                isMealNameContains = false;
+                break;
+            }
+        }
+        return isMealNameContains;
+    }
+
+    public List<Meal> filteringByIng(String[] ingredientsArray) {
+        List<Meal> filteredMealList = new ArrayList<>();
+        List<Meal> mainMealList = getMainMealList();
+        for (Meal meal : mainMealList) {
+            boolean isMealContainsIng = checkIngPerMeal(ingredientsArray, meal);
+            if (isMealContainsIng) {
+                filteredMealList.add(meal);
+            }
+        }
+        return filteredMealList;
+    }
+
+    private static boolean checkIngPerMeal(String[] ingArray, Meal meal) {
+        boolean isMealContainsIng = true;
+        ArrayList<Ingredient> ingredientList = meal.getIngredientList();
+        ArrayList<String> ingredientsNameList = new ArrayList<>();
+        ingredientList.forEach(ingredient -> ingredientsNameList.add(ingredient.getName()));
+        int overallCount = getOverallCount(ingArray, ingredientsNameList);
+        if (overallCount < ingArray.length) {
+            isMealContainsIng = false;
+        }
+        return isMealContainsIng;
+    }
+
+    private static int getOverallCount(String[] ingArray, ArrayList<String> ingredientsNameList) {
+        int overallCount = 0;
+        int zero = 0;
+        for (String ingName : ingArray) {
+            int eachCount = getEachCount(ingName, ingredientsNameList);
+            if (eachCount > zero) {
+                overallCount++;
+            }
+        }
+        return overallCount;
+    }
+
+    private static int getEachCount(String ingName, ArrayList<String> ingredientsNameList) {
+        int eachCount = 0;
+        for (String ingredientName : ingredientsNameList) {
+            if (ingredientName.contains(ingName)) {
+                eachCount++;
+            }
+        }
+        return eachCount;
     }
 }
--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -3,17 +3,26 @@ package seedu.parser;
 import seedu.command.ByeCommand;
 import seedu.command.Command;
 import seedu.command.CreateCommand;
+import seedu.command.FilterCommand;
+import seedu.command.SelectCommand;
 import seedu.command.UnknownCommand;
 
 public class Parser {
+    static String bye = "bye";
+    static String create = "create";
+    static String filter = "filter";
+    static String select = "select";
+
     public static Command parse(String userInput) {
-        String bye = "bye";
-        String create = "create";
         userInput = userInput.toLowerCase().trim();
-        if (userInput.contains(bye)) {
+        if (userInput.equalsIgnoreCase(bye)) {
             return new ByeCommand();
         } else if (userInput.contains(create)) {
             return new CreateCommand(userInput);
+        } else if (userInput.contains(filter)) {
+            return new FilterCommand(userInput);
+        } else if (userInput.contains(select)) {
+            return new SelectCommand(userInput);
         }
         return new UnknownCommand(userInput);
     }
