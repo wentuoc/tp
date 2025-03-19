5a9fc5c09a7c6e53fba59bb9bf1353c42480cd1b
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-19 10:21:16 +0800

    Update Parser.java

--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -5,6 +5,8 @@ import seedu.command.Command;
 import seedu.command.CreateCommand;
 import seedu.command.FilterCommand;
 import seedu.command.SelectCommand;
+import seedu.command.ListCommand;
+import seedu.command.MealCommand;
 import seedu.command.UnknownCommand;
 
 public class Parser {
@@ -12,17 +14,24 @@ public class Parser {
     static String create = "create";
     static String filter = "filter";
     static String select = "select";
+    static String meal = "meal";
+    static String list = "list";
 
     public static Command parse(String userInput) {
         userInput = userInput.toLowerCase().trim();
-        if (userInput.equalsIgnoreCase(bye)) {
+
+        if (userInput.startsWith(bye)) {
             return new ByeCommand();
-        } else if (userInput.contains(create)) {
+        } else if (userInput.startsWith(create)) {
             return new CreateCommand(userInput);
-        } else if (userInput.contains(filter)) {
+        } else if (userInput.startsWith(filter)) {
             return new FilterCommand(userInput);
-        } else if (userInput.contains(select)) {
+        } else if (userInput.startsWith(select)) {
             return new SelectCommand(userInput);
+        } else if (userInput.startsWith(list)) {
+            return new ListCommand();
+        } else if (userInput.startsWith(meal)) {
+            return new MealCommand();
         }
         return new UnknownCommand(userInput);
     }
