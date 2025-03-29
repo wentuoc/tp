166117e8d1b8b62f85fb5078cfdec0ce47ee5d48
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-03-30 02:20:18 +0800

    Edit the exceptions since some meals and ingredients can be free

--- src/main/java/seedu/exceptions/InvalidMcostException.java
+++ src/main/java/seedu/exceptions/InvalidMcostException.java
@@ -4,6 +4,6 @@ public class InvalidMcostException extends EZMealPlanException {
     @Override
     public String getMessage() {
         return "The /mcost input must be parsable into a double and the resulting double value " +
-                "must be more than 0.\n";
+                "must be more than or equals to 0.\n";
     }
 }
