e85237305515a7bcf44fa0ecbc65c43ff64717d4
wentuoc <e1113779@u.nus.edu>
2025-04-07 21:43:38 +0800

    Add logging for ConsumeCommandTest

--- src/test/java/seedu/command/ConsumeCommandTest.java
+++ src/test/java/seedu/command/ConsumeCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.command;
 
-import org.junit.jupiter.api.Test;
 import seedu.exceptions.EZMealPlanException;
 import seedu.exceptions.IngredientPriceFormatException;
 import seedu.exceptions.InvalidPriceException;
@@ -10,9 +9,20 @@ import seedu.food.Inventory;
 import seedu.logic.MealManager;
 import seedu.ui.UserInterface;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConsumeCommandTest {
+    private static final Logger logger = Logger.getLogger(ConsumeCommandTest.class.getName());
+
     UserInterface ui = new UserInterface();
     private final Ingredient ingredient1;
     private final Ingredient ingredient2;
@@ -24,10 +34,33 @@ class ConsumeCommandTest {
         ingredient2 = new Ingredient("Apple", "2.00");
         ingredient3 = new Ingredient("Banana", "3.00");
         ingredient4 = new Ingredient("Chocolate", "4.00");
+
+        String fileName = "ConsumeCommandTest.log";
+        setupLogger(fileName);
+    }
+
+    private static void setupLogger(String fileName) {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.INFO);
+        logger.addHandler(consoleHandler);
+        createLogFile(fileName);
+    }
+
+    private static void createLogFile(String fileName) {
+        try {
+            FileHandler fileHandler = new FileHandler(fileName, true);
+            fileHandler.setLevel(Level.FINE);
+            logger.addHandler(fileHandler);
+        } catch (IOException ioException) {
+            logger.log(Level.SEVERE, "File logger is not working.", ioException);
+        }
     }
 
     @Test
     public void testExecute_validInput_success() throws EZMealPlanException {
+        logger.fine("Running testExecute_validInput_success()");
         MealManager mealManager = new MealManager();
         Inventory inventory = mealManager.getInventory();
         inventory.addIngredient(ingredient1);
@@ -42,10 +75,12 @@ class ConsumeCommandTest {
                     2. Chocolate ($4.00): 1
                 """;
         assertEquals(expectedOutput, inventory.toString());
+        logger.info("Correct ingredient consumed");
     }
 
     @Test
     public void testExecute_validInputMultipleConsumed_success() throws EZMealPlanException {
+        logger.fine("Running testExecute_validInputMultipleConsumed_success()");
         MealManager mealManager = new MealManager();
         Inventory inventory = mealManager.getInventory();
         inventory.addIngredient(ingredient1);
@@ -59,10 +94,12 @@ class ConsumeCommandTest {
                     1. Chocolate ($4.00): 1
                 """;
         assertEquals(expectedOutput, inventory.toString());
+        logger.info("Correct ingredients consumed");
     }
 
     @Test
     public void testExecute_duplicateIngredientSamePrice_success() throws EZMealPlanException {
+        logger.fine("Running testExecute_duplicateIngredientSamePrice_success()");
         MealManager mealManager = new MealManager();
         Inventory inventory = mealManager.getInventory();
         inventory.addIngredient(ingredient1);
@@ -80,10 +117,12 @@ class ConsumeCommandTest {
                     3. Chocolate ($4.00): 1
                 """;
         assertEquals(expectedOutput, inventory.toString());
+        logger.info("Correct ingredient consumed");
     }
 
     @Test
     public void testExecute_duplicateIngredientDifferentPrice_exceptionThrown() throws EZMealPlanException {
+        logger.fine("Running testExecute_duplicateIngredientDifferentPrice_exceptionThrown()");
         MealManager mealManager = new MealManager();
         Inventory inventory = mealManager.getInventory();
         inventory.addIngredient(ingredient1);
@@ -101,15 +140,18 @@ class ConsumeCommandTest {
                     3. Chocolate ($4.00): 1
                 """;
         assertEquals(expectedOutput, inventory.toString());
+        logger.info("Correct ingredient consumed");
 
         String userInput2 = "consume /ing apple";
         Command command2 = new ConsumeCommand(userInput2);
 
         assertThrows(InventoryMultipleIngredientsException.class, () -> command2.execute(mealManager, ui));
+        logger.info("Correct exception thrown when attempting to remove ingredient with multiple prices");
     }
 
     @Test
     public void testExecute_duplicateIngredientDifferentPriceMultipleConsumed_exceptionThrown() {
+        logger.fine("Running testExecute_duplicateIngredientDifferentPriceMultipleConsumed_exceptionThrown()");
         MealManager mealManager = new MealManager();
         Inventory inventory = mealManager.getInventory();
         inventory.addIngredient(ingredient1);
@@ -117,9 +159,16 @@ class ConsumeCommandTest {
         inventory.addIngredient(ingredient2);
         inventory.addIngredient(ingredient3);
         inventory.addIngredient(ingredient4);
-        String userInput = "consume /ing apple, banana";
-        Command command = new ConsumeCommand(userInput);
+        String userInput1 = "consume /ing apple, banana";
+        Command command1 = new ConsumeCommand(userInput1);
 
-        assertThrows(InventoryMultipleIngredientsException.class, () -> command.execute(mealManager, ui));
+        assertThrows(InventoryMultipleIngredientsException.class, () -> command1.execute(mealManager, ui));
+        logger.info("Correct exception thrown");
+
+        String userInput2 = "consume /ing banana, apple";
+        Command command2 = new ConsumeCommand(userInput2);
+
+        assertThrows(InventoryMultipleIngredientsException.class, () -> command2.execute(mealManager, ui));
+        logger.info("Correct exception thrown");
     }
 }
