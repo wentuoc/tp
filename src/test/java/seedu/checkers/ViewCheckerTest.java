fd8a6eabdee81c59d5619ef0cc5e253c4bd9149b
RyanLing169 <ryanling12321@gmail.com>
2025-04-07 16:55:11 +0800

    fix list names

--- src/test/java/seedu/checkers/ViewCheckerTest.java
+++ src/test/java/seedu/checkers/ViewCheckerTest.java
@@ -12,38 +12,38 @@ import seedu.exceptions.InvalidViewIndexException;
 public class ViewCheckerTest {
 
     @Test
-    public void testValidViewInput_mainIndex_success() throws EZMealPlanException {
-        ViewChecker checker = new ViewChecker("view /m 2", "/m");
+    public void testValidViewInput_recipeIndex_success() throws EZMealPlanException {
+        ViewChecker checker = new ViewChecker("view /r 2", "/r");
         checker.check(); // Should not throw
     }
 
     @Test
-    public void testValidViewInput_userIndex_success() throws EZMealPlanException {
-        ViewChecker checker = new ViewChecker("view /u 1", "/u");
+    public void testValidViewInput_wishlistIndex_success() throws EZMealPlanException {
+        ViewChecker checker = new ViewChecker("view /w 1", "/w");
         checker.check(); // Should not throw
     }
 
     @Test
     public void testKeywordBeforeView_throwsInvalidKeywordIndexException() {
-        ViewChecker checker = new ViewChecker("/m view 1", "/m");
+        ViewChecker checker = new ViewChecker("/r view 1", "/r");
         assertThrows(InvalidKeywordIndexException.class, checker::check);
     }
 
     @Test
     public void testMissingMealIndex_throwsMissingMealIndexException() {
-        ViewChecker checker = new ViewChecker("view /m", "/m");
+        ViewChecker checker = new ViewChecker("view /r", "/r");
         assertThrows(MissingMealIndexException.class, checker::check);
     }
 
     @Test
     public void testNonIntegerIndex_throwsInvalidViewIndexException() {
-        ViewChecker checker = new ViewChecker("view /m abc", "/m");
+        ViewChecker checker = new ViewChecker("view /r abc", "/r");
         assertThrows(InvalidViewIndexException.class, checker::check);
     }
 
     @Test
     public void testExtraSpacingStillWorks() throws EZMealPlanException {
-        ViewChecker checker = new ViewChecker("   view    /u   4  ", "/u");
+        ViewChecker checker = new ViewChecker("   view    /w   4  ", "/w");
         checker.check(); // Should still succeed
     }
 }
