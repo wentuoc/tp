1f167618e01730738e81a572eef1b23e468df392
RyanLing169 <ryanling12321@gmail.com>
2025-04-07 19:09:39 +0800

    expand the imports

--- src/test/java/seedu/checkers/ViewCheckerTest.java
+++ src/test/java/seedu/checkers/ViewCheckerTest.java
@@ -3,7 +3,11 @@ package seedu.checkers;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
